01a1449d81a1355e8016a21e21c61cc6120dc2a5
ZT1996 <294238532@qq.com>
2016-10-23 17:31:20 +0800

    connect-with-owner

---
+++ app/api/user-book.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import {User, user_book} from '../mongodb/schema';
+
+const router = express.Router();
+router.get('/:id', function (req, res, next) {
+    const id = req.params.id;
+    user_book.findOne({id_book: id}, (err, docs)=> {
+        if (err) return next(err);
+        const userId = docs.id_user;
+        User.findOne({_id: userId}, (err, docs)=> {
+            if(err) return next(err);
+            return res.status(201).send(docs);
+        });
+    });
+});
+
+export default router;
