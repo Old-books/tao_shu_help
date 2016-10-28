e72ff5b92528d64ccf2f1019223dacdec2bfefd4
845968074 <845968074@qq.com>
2016-10-28 21:44:49 +0800

    tao

--- app/mongodb/db.js
+++ app/mongodb/db.js
@@ -3,9 +3,6 @@ module.exports = {
     connect: function (mode, callback) {
         /* let url = 'mongodb://localhost/tao-book-help';*/
         let url = process.env.PROD_MONGODB;
-        /*  if (mode === 'test') {
-         url = 'mongodb://localhost/tao-book-help-test';
-         }*/
         mongoose.connect(url, callback);
     },
     close: function (callback) {
